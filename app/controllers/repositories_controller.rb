d8ba3c604ad84bef1362d861ff9a6ba8cdf7cd15
Roshan Choxi <roshan.choxi@gmail.com>
2016-01-23 17:01:47 -0800

    table of repos by year

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,4 +2,9 @@ class RepositoriesController < ApplicationController
   def index
     @repositories = Repository.limit(10000).all
   end
+
+  def show
+    db = Repository.connection
+    @repos_by_year = db.execute("SELECT date_trunc('day', created_at), COUNT(*) FROM repositories GROUP BY 1")
+  end
 end
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,2 +1,5 @@
 class Repository < ActiveRecord::Base
+  def self.count_by_year
+    
+  end
 end
