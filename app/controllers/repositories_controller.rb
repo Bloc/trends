dc4a1548adb328b2de18f0a498531e1ed6bddc2e
Roshan Choxi <roshan.choxi@gmail.com>
2016-01-24 22:33:20 -0800

    add total count + recently updated count on homepage

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,8 @@
 class RepositoriesController < ApplicationController
   def index
-    @repositories = Repository.order("created_at DESC").limit(1_000).all
+    @total_indexed_count  = Repository.all.count
+    @fresh_count          = Repository.fresh.count
+    @repositories         = Repository.fresh.order("created_at DESC").limit(1_000)
   end
 
   def show
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,4 +1,5 @@
 class Repository < ActiveRecord::Base
   scope :stale,     -> { where("last_synced_at IS NULL OR last_synced_at < ?", 30.days.ago) }
+  scope :fresh,     -> { where("last_synced_at IS NOT NULL AND last_synced_at > ?", 30.days.ago) }
   scope :not_fork,  -> { where(fork: false) }
 end
