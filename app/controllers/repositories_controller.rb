25dd161e1521fe19b7db7404322284e8709d2865
Roshan Choxi <roshan.choxi@gmail.com>
2016-01-24 23:01:49 -0800

    clean up UI

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,7 +7,7 @@ class RepositoriesController < ApplicationController
 
   def show
     db = Repository.connection
-    @repos_by_month = db.execute("SELECT date_trunc('month', created_at), COUNT(*) FROM repositories WHERE created_at IS NOT NULL GROUP BY 1")
+    @repos_by_month = db.execute("SELECT date_trunc('month', created_at), COUNT(*) FROM repositories WHERE last_synced_at IS NOT NULL GROUP BY 1 ORDER BY 1")
     gon.years = @repos_by_month.map {|row| Time.parse(row["date_trunc"]).strftime("%b %Y") }
     gon.repos = @repos_by_month.map {|row| row["count"].to_i }
   end
