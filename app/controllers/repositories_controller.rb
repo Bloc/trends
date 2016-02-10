9eea9bfcdecea26391271b6ca424aa78ae361192
Roshan Choxi <roshan.choxi@gmail.com>
2016-02-09 22:39:09 -0800

    comparsion chart on homepage

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,6 +3,15 @@ class RepositoriesController < ApplicationController
     @total_indexed_count  = Repository.all.count
     @fresh_count          = Repository.fresh.count
     @repositories         = Repository.fresh.order("created_at DESC").limit(1_000)
+
+    js_repos    = Repository.repos_by_month(language: "JavaScript")
+    ruby_repos  = Repository.repos_by_month(language: "Ruby")
+
+    gon.years   = ruby_repos.map {|row| Time.parse(row["date_trunc"]).strftime("%b %Y") }
+    gon.series  = [
+      { name: "JavaScript", data: js_repos.map {|row| row["count"].to_i } },
+      { name: "Ruby",       data: ruby_repos.map {|row| row["count"].to_i } }
+    ]
   end
 
   def show
